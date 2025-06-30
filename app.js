38b8eab17b7174948acf30d4a8aa28ed84c6ec7e
Kaz-Kazuto <kirikayakazuto413@gmail.com>
2025-06-30 21:31:35 +0700

    product finish

--- app.js
+++ app.js
@@ -4,27 +4,6 @@ let carousel = document.querySelector(".carousel");
 let listHTML = document.querySelector(".carousel .list");
 let seeMoreButtons = document.querySelectorAll(".seeMore");
 let backButton = document.getElementById("back");
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
 
 nextButton.onclick = function () {
   showSlider("next");
